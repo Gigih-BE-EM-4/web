b8f9856aec8d63650064943baa683befeb12f4d5
Kadek Rizky Fransisca Putra <rizky.royal@gmail.com>
2022-06-08 20:31:01 +0700

    Add Company Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Company;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,34 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        Company::create([
+            "name" => "Golek",
+            "profile" => "img.jpg",
+            "bio" => "Golek is the best bro",
+            "address" => "Jakarta Selatan",
+            "category" => "Technology",
+            "email" => "golek@gmail.com",
+            "contact" => "081938713808"
+        ]);
+
+        Company::create([
+            "name" => "TokuToku",
+            "profile" => "img.jpg",
+            "bio" => "TokuToku is the best bro",
+            "address" => "Jakarta Selatan",
+            "category" => "Technology",
+            "email" => "tokutoku@gmail.com",
+            "contact" => "081938713808"
+        ]);
+
+        Company::create([
+            "name" => "BukaBuku",
+            "profile" => "img.jpg",
+            "bio" => "BukaBuku is the best bro",
+            "address" => "Jakarta Selatan",
+            "category" => "Technology",
+            "email" => "bukabuku@gmail.com",
+            "contact" => "081938713808"
+        ]);
     }
 }
